c5a83a5fe52ed4fc823c599a136dc486e9701d68
Anuj5504 <anujjadhav0505@gmail.com>
2024-12-18 23:40:56 +0530

    cache removal

--- backend/controllers/userController.ts
+++ backend/controllers/userController.ts
@@ -8,6 +8,7 @@ import path from "path";
 import ejs from "ejs";
 import sendMail from "../utils/sendMails";
 import { sendToken } from "../utils/jwt";
+import { redis } from "../utils/redis";
 
 //register User
 
@@ -160,6 +161,9 @@ export const logout = catchAsyncError(async (req: Request, res: Response, next: 
     try {
         res.cookie("access_token", "", { maxAge: 1 });
         res.cookie("refresh_token", "", { maxAge: 1 });
+
+        const userId = req.user?._id || "";
+        redis.del(userId as string);
 
         res.status(200).json({
             success: true,
--- backend/routes/userRoute.ts
+++ backend/routes/userRoute.ts
@@ -1,5 +1,6 @@
 import  express  from "express";
 import { activateUser, loginUser, logout, registerationUser } from "../controllers/userController";
+import { authorizedRoles, isAuthenticated } from "../middleware/auth";
 const userRouter=express.Router();
 
 userRouter.post('/registration',registerationUser);
@@ -8,6 +9,7 @@ userRouter.post('/activate-user',activateUser);
 
 userRouter.post('/login',loginUser);
 
-userRouter.get('/logout',logout);
+userRouter.get('/logout',isAuthenticated,authorizedRoles("admin"),logout);
+
 
 export default userRouter;
